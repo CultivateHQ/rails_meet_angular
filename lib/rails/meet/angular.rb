586ec01b1bc70b2e2197e7799c0e5e175627e805
Caden Lovelace <caden@herostrat.us>
2014-08-14 10:09:35 +0100

    Initial commit.

---
+++ lib/rails/meet/angular.rb
@@ -0,0 +1,9 @@
+require "rails/meet/angular/version"
+
+module Rails
+  module Meet
+    module Angular
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/meet/angular/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Meet
+    module Angular
+      VERSION = "0.0.1"
+    end
+  end
+end
