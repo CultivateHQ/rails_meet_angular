8ecfe50fd87e87859cfec11b8277c075c85dd9a9
Caden Lovelace <caden@herostrat.us>
2014-08-19 17:23:14 +0100

    Remove unnecessary precompile initialiser.

--- lib/rails_meet_angular/engine/railtie.rb
+++ lib/rails_meet_angular/engine/railtie.rb
@@ -1,7 +1,4 @@
 module RailsMeetAngular
   class Engine < ::Rails::Engine
-    initializer 'rails_meet_angular.precompile_hook', group: :all do |app|
-      app.config.assets.precompile << 'templates.js.erb'
-    end
   end
 end
