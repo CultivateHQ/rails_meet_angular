acc8f798d7060ba88152db969f82256d59bb5526
Caden Lovelace <caden@herostrat.us>
2014-08-20 10:25:36 +0100

    Include ngannotate-rails and require slim.

--- lib/rails_meet_angular.rb
+++ lib/rails_meet_angular.rb
@@ -2,5 +2,7 @@ require "rails_meet_angular/version"
 require "rails_meet_angular/helpers"
 require "rails_meet_angular/railtie" if defined? Rails
 require "rails_meet_angular/engine/railtie" if defined? Rails
+require "ngannotate/rails" if defined? Rails
+require "slim" if defined? Rails
 
 module RailsMeetAngular; end
--- rails_meet_angular.gemspec
+++ rails_meet_angular.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3.0.0"
   spec.add_dependency "rails", "~> 4.0"
   spec.add_dependency "slim"
+  spec.add_dependency "ngannotate-rails"
 end
